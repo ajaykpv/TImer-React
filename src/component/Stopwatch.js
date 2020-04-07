a5fad78c7e066a64ba0036bf73c950f292464def
Ajay Krishnan <you@example.com>
2020-04-08 00:38:44 +0530

    Laptime is added

--- src/component/Stopwatch.js
+++ src/component/Stopwatch.js
@@ -14,6 +14,14 @@ class Stopwatch extends Component {
           timerStart: Date.now() - this.state.timerTime
           
         });
+        if(this.state.timerTime===0){
+            var x= document.createElement('textarea')
+            x.setAttribute("id","laptime")
+            x.setAttribute("rows",5)
+            x.setAttribute("cols",25)
+            document.getElementById("lap").appendChild(x)
+        }
+        
         
         this.timer = setInterval(() => {
           this.setState({
@@ -30,7 +38,22 @@ class Stopwatch extends Component {
           timerStart: 0,
           timerTime: 0
         });
+        document.getElementById('lap').innerHTML=""
       };
+      lapTime=()=>{
+          this.stopTimer()
+        const { timerTime } = this.state;
+        let centiseconds = ("0" + (Math.floor(timerTime / 10) % 100)).slice(-2);
+        let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
+        let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
+        let hours = ("0" + Math.floor(timerTime / 3600000)).slice(-2);
+        this.startTimer()
+       
+            console.log(`${hours} : ${minutes} : ${seconds} : ${centiseconds}`)
+            var y=document.getElementById("laptime").value
+            document.getElementById('laptime').innerHTML= ` ${hours} : ${minutes} : ${seconds} : ${centiseconds} &#13;&#10; `+ y
+    
+      }
     render() {
         const { timerTime } = this.state;
         let centiseconds = ("0" + (Math.floor(timerTime / 10) % 100)).slice(-2);
@@ -42,19 +65,30 @@ class Stopwatch extends Component {
         <div className="Stopwatch-header">
             <div className="Stopwatch-display">
                 {hours} : {minutes} : {seconds} : {centiseconds}<br/>
-                {this.state.timerOn === false && this.state.timerTime === 0 && (
+               
+                { this.state.timerOn === false && this.state.timerTime === 0 && (
+                        
                         <button onClick={this.startTimer}><a href="#">START</a></button>
                         )}
-                        {this.state.timerOn === true && (
-                        <button onClick={this.stopTimer}><a href="#">STOP</a></button>
-                        )}
-                        {this.state.timerOn === false && this.state.timerTime > 0 && (
-                        <button onClick={this.startTimer}><a href="#" >RESUME</a></button>
-                        )}
-                        {this.state.timerOn === false && this.state.timerTime > 0 && (
-                        <button onClick={this.resetTimer}><a href="#">RESET</a></button>
-                        )}
+                {this.state.timerOn === true && (
+                <button onClick={this.stopTimer}><a href="#">STOP</a></button>
+                         )}
+                {this.state.timerOn === true && (
+                <button onClick={this.lapTime}><a href="#">LAPTIME</a></button>
+                         )}
+                {this.state.timerOn === false && this.state.timerTime > 0 && (
+                <button onClick={this.startTimer}><a href="#" >RESUME</a></button>
+                         )}
+                {this.state.timerOn === false && this.state.timerTime > 0 && (
+                <button onClick={this.resetTimer}><a href="#">RESET</a></button>
+                         )}
+                         <br/>
+                         
+                         
             </div>
+            <div id ="lap">
+
+                         </div>
             </div>
       </div>
     );
